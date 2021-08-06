720f7218e2b7faf6c5075626e969dd489061fb22
lorenzodallamuta <lorenzodallamuta@gmail.com>
2021-08-06 23:31:23 +0200

    include django-cors-headers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_filters',
+    'corsheaders',
     'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -134,3 +136,9 @@ REST_FRAMEWORK = {
         'rest_framework_filters.backends.RestFrameworkFilterBackend',
     ),
 }
+
+# CORS headers settings (django-cors-headers)
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+    'http://172.24.223.215:8080'
+]
